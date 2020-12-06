d0dbbafdc8df461ff643ec39a201e6fd22ddf4b5
Otkrovennost <Otkrovennost@mail.ru>
2020-12-06 22:15:38 +0300

    feat: task-1-js

---
+++ task-js-1/task-3.js
@@ -0,0 +1,12 @@
+// Bracket Duplicates
+// Desc: Create a program that will take in a string as input and, 
+// if there are duplicates of more than two alphabetical characters in the string, 
+// returns the string with all the extra characters in a bracket.
+// Note: ensure that the input is a string, and return "Please enter a valid string" if it is not.
+// Given: "aaaabbcdefffffffg" 
+// Return: "aa[aa]bbcdeff[fffff]g" 
+
+// Example
+// Test.assertEquals(stringParse("aaaabbcdefffffffg"), "aa[aa]bbcdeff[fffff]g")
+// Test.assertEquals(stringParse("boopdedoop"), "boopdedoop")
+// Test.assertEquals(stringParse("helloookat"), "helloo[o]kat")
